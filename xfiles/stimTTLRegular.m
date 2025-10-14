25e9c197cb9e7afb79e94ec7af0b8bd804e8777e
Daisuke Shimaoka <daisuke.shimaoka@monash.edu>
2025-10-14 14:06:57 +1100

    overhaul stimTTLRegular

--- xfiles/stimTTLRegular.m
+++ xfiles/stimTTLRegular.m
@@ -21,7 +21,7 @@ pp{1} = {'nPtn', '#images per condition',1,1,100000};
 pp{2} = {'ptnIdx', 'Pattern ID',1,1,100000}; %must be identical to condition number ... no need to provide as an input
 pp{3}  = {'WaveAmp',   'Amplitude of wave (mV*1000)',    100, 0, 5000};
 pp{4}  = {'WaveFreq',  'Frequency (Hz)',             4400,0,20000};
-pp{5}  = {'WaveDurOn',     'Duration of ON period per image (ms)',   20,0,1000}; 
+pp{5}  = {'WaveDuty',     'percentage of ON period per presentation(%)',   50,0,100}; 
 x = XFile('stimTTLRegular',pp);
 % x.Write; % call this ONCE: it writes the .x file
 
@@ -36,16 +36,11 @@ nPtn = Pars(1); %number of patterns per condition registered in polyScan. MUST B
 PtnIdx = Pars(2);
 WaveAmp = Pars(3)/1000; %V
 WaveFreq = Pars(4);    % Hz
-WaveDurOn   = Pars(5);    % ms
+WaveDuty   = Pars(5);    % [%]
 
 margin = 1; %number of blank frames before/after TTL switching ... could be just 0?
-
-dur = nPtn/WaveFreq; %s
+wavedur = nPtn/WaveFreq; %s
 fs = 5000;
-nt = ceil(dur*fs);
-% tt = (1:nt)./fs;
-
-disp(['stimTTLRegular: stim duration ' num2str(dur) '(s)']);
 
 %% Make the stimulus
 
@@ -53,7 +48,17 @@ SS = ScreenStim; % initialization
 SS.Type = 'stimTTLRegular';
 SS.Parameters = Pars;
 
+nConds = size(SS.Parameters,2);
+WaveStart   = ceil(2*1000*nConds*nPtn/fs);%Pars(5);    % ms
+WaveStop    = WaveStart+wavedur*1000;%Pars(6);    % ms
+WaveDurOn_TTL   = 2;%Pars(8);    % ms % to switch DMD images
 
+ntWavestart = ceil(WaveStart/1000*fs);
+ntWavestop = ceil(WaveStop/1000*fs);
+nt = ntWavestart+ntWavestop;
+dur = ceil(nt/fs);
+tt = (1:nt)./fs; %[s] consecutive time
+ttWave = (1:ntWavestop-ntWavestart-1)./fs; %[s] consecutive time after wave start
 
 % a blank visual stimulus
 SS.nTextures = 1;
@@ -66,46 +71,30 @@ SS.ImageSequence = ones(1,SS.nFrames);
 SS.SourceRects = repmat([1; 1; 1; 1],[1 1 SS.nFrames]);
 SS.DestRects   = repmat([1; 1; 1; 1],[1 1 SS.nFrames]);
 
+
 SS.WaveStim.Waves = zeros(nt,2); 
 
-nConds = size(SS.Parameters,2);
-WaveStart   = ceil(2*1000*nConds*nPtn/fs);%Pars(5);    % ms
-WaveStop    = WaveStart+dur*1000;%Pars(6);    % ms
-WaveDurOn_TTL   = 2;%Pars(8);    % ms % to switch DMD images
-
-ntWavestart = ceil(WaveStart/1000*fs);
-ntWavestop = ceil(WaveStop/1000*fs);
-ttWave = (1:ntWavestop-ntWavestart-1)./fs; %[s]
-
 %% wave
-if WaveDurOn==0 %constant ON 
-    SS.WaveStim.Waves(ntWavestart:ntWavestop-2,1) = WaveAmp;
-else
-    duty = WaveDurOn/1000*WaveFreq*100; %on duration in one cycle [%]
-    SS.WaveStim.Waves(ntWavestart:ntWavestop-2,1) = ...
-        WaveAmp*square(2*pi*WaveFreq*ttWave, duty)';
-end
+SS.WaveStim.Waves(ntWavestart:ntWavestop-2,1) = ...
+    0.5*WaveAmp*(square(2*pi*WaveFreq*ttWave, WaveDuty)+1)';
+
 
 %% TTL
 %before Wave, fast forward to the x-th ptn
 nTTL_before = (PtnIdx-1)*nPtn;% - 1;
 if nTTL_before >= 1
-    assert(ntWavestart-margin-2*nTTL_before+1>0, 'Not enough time for Ptn switching. Increase WaveStart');
     SS.WaveStim.Waves(ntWavestart-margin-2*nTTL_before+1:2:ntWavestart-margin,2) = 5;
 end
 
 %after Wave, reset to the 1st ptn
-% nTTL_after  = nPtn - PtnIdxp;% + 1;
 nTTL_after  = nConds*nPtn - PtnIdx*nPtn;% + 1;
 if nTTL_after >= 1
-    assert(ntWavestop+margin+2*nTTL_after-1<nt, 'Not enough time for Ptn switching. Increase dur or decrease WaveStop');
-        SS.WaveStim.Waves(ntWavestop+margin:2:ntWavestop+margin+2*nTTL_after-1,2) = 5;
+    SS.WaveStim.Waves(ntWavestop+margin:2:ntWavestop+margin+2*nTTL_after-1,2) = 5;
 end
 
 %regular freq during trial
 SS.WaveStim.Waves(ntWavestart:ntWavestop-2,2) = ...
-    2.5*(square(2*pi*WaveFreq*ttWave, WaveDurOn_TTL/1000*WaveFreq*100)-1)';
-
+    2.5*(square(2*pi*WaveFreq*ttWave, WaveDurOn_TTL/1000*WaveFreq*100)+1)';
 
 
 SS.WaveStim.SampleRate = fs;
@@ -115,6 +104,14 @@ SS.MinusOneToOne = 0;
 return
 
 %% To test the code
+
+% ax(1) = subplot(211);
+% plot(tt, SS.WaveStim.Waves(:,1));vline([WaveStart WaveStop]/1000,ax(1));
+% 
+% ax(2) = subplot(212);
+% plot(tt, SS.WaveStim.Waves(:,2));vline([WaveStart WaveStop]/1000,ax(2));
+% 
+% linkaxes(ax);
 
 SS = stimTTLRegular(myScreenInfo); %#ok<UNRCH>
 SS.Show(myScreenInfo)
--- xfiles/stimTTLRegular.x
+++ xfiles/stimTTLRegular.x
@@ -7,6 +7,6 @@ foo nonperiodic	5
 0	nPtn	"                    Number of patterns per condition"	1	1-100000 1+
 1	ptnIdx	"                    Pattern Idx to present"	1	1-100000 1+
 2	WaveAmp	"         Amplitude of rectangle wave (mV*1000)"	100	0-5000 1+
-3	WaveFreq	"          Wave Frequency (Hz*10)"	1	0-20000 1+
-4	WaveDurOn	"           Wave On duration (ms)"	1000	0-10000 1
+3	WaveFreq	"          Refresh rate (Hz)"	1	0-20000 1+
+4	WaveDuty	"           Image ON ratio per presentation(%)"	50	0-100 1
 
